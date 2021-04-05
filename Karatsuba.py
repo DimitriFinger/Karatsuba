08337f1c6775ea3c6ebd37786a9f2ae84746e6ce
Dimitri Finger <dimitri.finger@outlook.com>
2021-04-05 02:09:11 -0300

    Sub/Add added

--- Karatsuba.py
+++ Karatsuba.py
@@ -1,27 +1,118 @@
 # Author : Dimitri da Silva Finger
 # Projeto e Otimização de Algoritmos - 2021/1
 # Pontifícia Universidade Católica do Rio Grande do Sul (PUC-RS)
-# Algoritmo de Karatsuba
+# Karatsuba's Algorithm
 
 import sys
 
-
-def CheckSize(number_one, number_two):
+def Karatsuba(number_A, number_B):
     
-    if len(number_one) > len(number_two):
-        while len(number_one) != len(number_two):
-            number_two = '0' + number_two
-
-    elif len(number_one) < len(number_two):
-        while len(number_one) != len(number_two):
-            number_one = '0' + number_one
     
 
-    if len(number_one)%2 != 0:
-        number_one = '0' + number_one
-        number_two = '0' + number_two
+    # define shift size
+    shift = int(len(number_A)/2) * '0'
 
-    return number_one, number_two
+    if len(number_A) == 1:
+        return str(int(number_A) * int(number_B))
+
+    if len(number_A) > len(number_B):
+        while len(number_A) != len(number_B):
+            number_B = '0' + number_B
+
+    elif len(number_A) < len(number_B):
+        while len(number_A) != len(number_B):
+            number_A = '0' + number_A    
+
+    if len(number_A)%2 != 0 and len(number_A) > 1:
+        number_A = '0' + number_A
+        number_B = '0' + number_B  
+
+    #Sum(number_A,number_B)
+    Subtraction(number_A, number_B)
+    
+    number_A_1 = number_A[:int(len(number_A)/2)]
+    number_A_2 = number_A[int(len(number_A)/2):]
+    number_B_1 = number_B[:int(len(number_B)/2)]
+    number_B_2 = number_B[int(len(number_B)/2):]
+
+
+def Sum(number_1, number_2):
+
+    x = number_1
+    y = number_2
+    aux = 0
+    result = ''
+
+    while len(x) != 0:
+        calc = int(x[-1:]) + int(y[-1:]) + aux
+        result = str(calc%10) + result
+        aux = int(calc/10)
+        x = x[:-1]
+        y = y[:-1]
+    
+    return print(str(result))
+
+
+
+def Subtraction(number_1,number_2):
+    x = number_1
+    y = number_2
+    result = ''
+    aux2 = 0
+    changed = False
+
+    if y > x :
+        x,y = y,x
+        changed = True
+
+    while len(x) != 0:
+        if y[-1:] > x[-1:]:
+            aux = int(x[-1:]) + 10
+            aux = aux - int(y[-1:])
+            aux2 = 1
+            result = str(aux) + result
+        else:
+            aux = int(x[-1:]) - aux2 - int(y[-1:])
+            aux2 = 0
+            result = str(aux) + result
+    
+        x = x[:-1]
+        y = y[:-1]
+    
+    #if changed == False:
+    print(result)
+    
+
+
+
+
+
+
+
+
+
+
+
+ 
+    #part1 = Karatsuba(number_A_1,number_B_1)
+    #part2 = Karatsuba(number_A_2,number_B_1)
+    #part3 = Karatsuba(number_A_1,number_B_2)
+    #part4 = Karatsuba(number_A_2,number_B_2)
+
+    #print(part1)
+    
+    #AB = (str(part1) + 2*shift) + (str(int(part2+part3)) + shift) + str(part4)
+    #print(AB)
+    #print(part1,part2,part3,part4)
+
+
+
+    
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -34,12 +125,14 @@ if __name__ == "__main__":
         sys.exit(1)
         
     else:
-        number_one = sys.argv[1]
-        number_two = sys.argv[2]
+        number_A = sys.argv[1]
+        number_B = sys.argv[2]
 
-    x,y = CheckSize(number_one,number_two)
+    Karatsuba(number_A,number_B)
    
-    print(x,y)
+    #print(x,y,z)
+    #print(len(x),len(y),len(z))
+    #print(type(x), type(y),type(z))
 
    
 
