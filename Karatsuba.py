ffeeab13db5d222c1493a4acbc4662043731495c
Dimitri Finger <dimitri.finger@outlook.com>
2021-04-05 23:45:24 -0300

    .

--- Karatsuba.py
+++ Karatsuba.py
@@ -7,17 +7,11 @@ def Karatsuba(number_A, number_B):
        return str(int(number_A) * int(number_B))
 
     number_A,number_B = SameSize(number_A,number_B)
-    #print(len(number_A)/2)
-    #print(number_A, number_B)
-    #print(type(number_A), type(number_B))
+   
     shift = int(len(number_A)/2) * '0'
-    #print(type(shift))
+    
     a1,a2,b1,b2 = Slice(number_A, number_B)
-    #print(type(a1), type(a2),type(b1), type(b2))
-    #print(a1,a2 + "   " + b1,b2)
-    #teste_soma=Sum(number_A,number_B)
-    #print(type(teste_soma))
-    #teste_sub=Sub(number_A, number_B)
+    
     a1,a2 = SameSize(a1,a2)
     b1,b2= SameSize(b1,b2)
     a1a2 = Sum(a1,a2)
@@ -26,11 +20,10 @@ def Karatsuba(number_A, number_B):
     a1_b1 = Karatsuba(a1,b1)
     a2_b2 = Karatsuba(a2,b2)
     a1a2_b1b2 = Karatsuba(a1a2,b1b2)
-    #print("a1a2 = " + a1a2)
-    #print("b1b2 = " + b1b2)
+    
     a1a2_b1b2,a1_b1= SameSize(a1a2_b1b2,a1_b1)
     subtraction = Sub(a1a2_b1b2,a1_b1)
-    #print(subtraction, a2_b2)
+    
     subtraction, a2_b2= SameSize(subtraction, a2_b2)
     subtracted_part = Sub(subtraction, a2_b2)
 
@@ -61,8 +54,7 @@ def SameSize(number_A, number_B):
         number_B = '0' + number_B     
 
     return number_A,number_B
-    #print(number_A, number_B)
-    #print(type(number_A), type(number_B))
+    
 
 
 def Slice(number_A, number_B):
@@ -70,46 +62,38 @@ def Slice(number_A, number_B):
     a2 = number_A[int(len(number_A)/2):]
     b1 = number_B[:int(len(number_B)/2)]
     b2 = number_B[int(len(number_B)/2):]
-    #print(a1,a2 + "   " + b1,b2)
+    
     return a1,a2,b1,b2
 
 
-def Sum(number_1,number_2):
-    #print(number_1,number_2)
+def Sum(number_1,number_2):    
     carry = 0
     result = ''
     
     while len(number_1) != 0:
-        calc = int(number_1[-1]) + int(number_2[-1]) + carry
-        #print(calc)
+        calc = int(number_1[-1]) + int(number_2[-1]) + carry        
         result = str(calc%10) + result       
         carry = int(calc/10)    
         number_1 = number_1[:-1]
-        number_2 = number_2[:-1]   
+        number_2 = number_2[:-1]
 
     if carry != 0:
         result = str(carry) + result
-    #print(type(result))
     return result
 
+
+
 def Sub(number_1,number_2):
-    #print(number_1,number_2)
-    #print(type(number_1),type(number_2))
-    #print(len(number_1))
     carry = 0
     result = ''
     aux = 0
     if number_2 > number_1 :
         number_1,number_2 = number_2,number_1        
-    #print(type(number_1),type(number_2))
-    #print(number_1,number_2)
-
+    
     if len(number_2) == 1:
         result = str(int(number_1[-1]) - int(number_2[-1]))
-        #print(result)
     else:
         while len(number_1) !=0 :
-                    #print(number_1, number_2)
                     if number_2[-1] > number_1[-1]:                        
                         aux = int(number_1[-1]) + 10  - carry             
                         aux = aux - int(number_2[-1])
@@ -158,7 +142,3 @@ if __name__ == "__main__":
     x = int(Karatsuba(number_A,number_B))
     print(str(x))
    
-    #print(number_A)
-    #print(number_B)
-    #print(type(number_A))
-    #print(type(number_B))
